65510d604122e0928578e3d431a57c832ce4b7b6
EpicZuko <zukokalilov5@gmail.com>
2024-01-25 23:38:06 +0600

    created validation

---
+++ src/components/UI/InputCodeValidation.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const InputCodeValidation = (props) => {
+   return (
+      <StyledDiv>
+         <StyledInput
+            type="number"
+            min="0"
+            onChange={props.onChange}
+            value={props.value}
+         />
+         {props.value === '' && <StyledSpan />}
+      </StyledDiv>
+   )
+}
+
+export default InputCodeValidation
+
+const StyledDiv = styled.div`
+   position: relative;
+`
+const StyledInput = styled.input`
+   display: flex;
+   width: 56px;
+   height: 64px;
+   padding: 13px 16px;
+   text-align: center;
+   justify-content: center;
+   align-items: center;
+   gap: 8px;
+   color: var(--Grey-Extra-Dark, #212121);
+   font-family: 'Mplus 1p';
+   font-size: 50px;
+   font-style: normal;
+   font-weight: 500;
+   line-height: 24px;
+   &[type='number']::-webkit-inner-spin-button,
+   &[type='number']::-webkit-outer-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+   }
+
+   &[type='number'] {
+      -moz-appearance: textfield;
+   }
+`
+const StyledSpan = styled.span`
+   position: absolute;
+   text-align: center;
+   top: 70px;
+   left: 35px;
+   border-bottom: 5px solid var(--Grey-Extra-Dark, #212121);
+   width: 24px;
+`
