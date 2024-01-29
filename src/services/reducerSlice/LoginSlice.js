cd9f421517b869404da997e3b4542af0389ee985
EpicZuko <zukokalilov5@gmail.com>
2024-01-29 17:08:49 +0600

    created login

--- src/services/reducerSlice/LoginSlice.js
+++ src/services/reducerSlice/LoginSlice.js
@@ -4,6 +4,7 @@ import {
    showErrorMessage,
    showSuccessMessage,
 } from '../../utills/helpers/helpers'
+import { LocalStorageFunction } from '../../utills/helpers/LocalStorageFunction'
 
 export const authPost = createAsyncThunk(
    'login/getUserOrAdmin',
@@ -91,3 +92,63 @@ export const codeEmailSlice = createSlice({
    },
 })
 export const codeEmailAction = codeEmailSlice.actions
+
+export const loginPost = createAsyncThunk(
+   'loginPost/loginPostServer',
+   // eslint-disable-next-line consistent-return
+   async (props, { rejectWithValue }) => {
+      try {
+         const response = await ApiFetch({
+            url: 'authentication/login/',
+            method: 'POST',
+            body: props.code,
+         })
+         LocalStorageFunction({
+            type: 'removeItem',
+            key: 'login',
+         })
+         LocalStorageFunction({
+            type: 'setItem',
+            key: 'login',
+            body: {
+               refresh: response.refresh,
+               access: response.accessF,
+            },
+         })
+
+         showSuccessMessage('Вы успешно авторизовались!')
+         return response
+      } catch (error) {
+         showErrorMessage('Неверный логин или пароль')
+         return rejectWithValue(error)
+      }
+   }
+)
+
+const initialStateLogin = {
+   status: null,
+   error: null,
+   refresh: null,
+   access: null,
+}
+
+export const loginLorbySlice = createSlice({
+   name: 'loginPost',
+   initialState: initialStateLogin,
+   reducers: {},
+   extraReducers: (builder) => {
+      builder
+         .addCase(loginPost.pending, (state) => {
+            state.status = 'pending'
+         })
+         .addCase(loginPost.fulfilled, (state, action) => {
+            state.status = 'success'
+            state.refresh = action.payload.refresh
+            state.access = action.payload.access
+         })
+         .addCase(loginPost.rejected, (state) => {
+            state.error = 'error'
+         })
+   },
+})
+export const loginAction = loginLorbySlice.actions
