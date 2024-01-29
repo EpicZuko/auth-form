192de5a23cf90e54efaa081284b910dacf88eca3
EpicZuko <zukokalilov5@gmail.com>
2024-01-29 19:11:20 +0600

    created page register

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
-// import CreateAccount from './components/CreateAccount'
-// import EnterTheCode from './components/EnterTheCode'
-// import Login from './components/Login'
-
-import RegisterPage from './components/register/RegisterPage'
-
 function App() {
-   return (
-      <div>
-         {/* <CreateAccount /> */}
-         {/* <EnterTheCode /> */}
-         {/* <Login /> */}
-         <RegisterPage />
-      </div>
-   )
+   return <div />
 }
 
 export default App
